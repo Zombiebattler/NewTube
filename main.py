8872ceb7886a27a1e341e971a239ca1d4ff87cbd
Zombiebattler <82572053+Zombiebattler@users.noreply.github.com>
2023-10-29 20:22:45 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/<path:url>')
+def embed_youtube(url):
+    full_url = request.full_path
+    video_id = full_url.replace("/https://www.youtube.com/watch?v=", "")
+    embed_code = f'<iframe width="100%" height="100%" src="https://www.youtube.com/embed/{video_id}" frameborder="0" allowfullscreen></iframe>'
+    return render_template('index.html', video_embed=embed_code)
+
+if __name__ == '__main__':
+    app.run(port=6969)
