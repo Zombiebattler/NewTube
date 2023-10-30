ae8e494b73aab4bb1229bb329a87e54d7f791ef8
Leopold <93437384+DEanozyp@users.noreply.github.com>
2023-10-30 12:01:09 +0100

    new desing

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def embed_youtube(url):
     video_id = full_url.replace("/https://www.youtube.com/watch?v=", "")
     if "&t=" in video_id:
         video_id = re.sub(pattern, '', video_id)
+    
     else:
         pass
     embed_code = f'<iframe width="100%" height="100%" src="https://www.youtube.com/embed/{video_id}" frameborder="0" allowfullscreen></iframe>'
