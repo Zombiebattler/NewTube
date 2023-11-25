a5ea1a0e85d61e0c331144093e75f7183ac8161c
Zombiebattler <82572053+Zombiebattler@users.noreply.github.com>
2023-11-25 19:50:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,61 @@
 from flask import Flask, request, render_template, jsonify
+from pytube import YouTube
+import logging
 import re
-try:
-    from pytube import YouTube
-except Exception:
-    pass
-
 
 pattern = r'&t=\d+s'
-
 app = Flask(__name__)
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
-def load_likes():
-    try:
-        with open("static/likes.txt", "r") as file:
-            likes = [line.strip() for line in file]
-        return likes
-    except FileNotFoundError:
-        return []
-
-def save_likes(likes):
-    with open("static/likes.txt", "w") as file:
-        for like in likes:
-            file.write(like + "\n")
-
-@app.route('/beta/add_like/<path:id>')
-def add_like(id):
-    if not "https://www.youtube.com/watch" in id:
-        url = f"https://www.youtube.com/watch?v={id}"
-        likes = load_likes()
-        if url not in likes:
-            likes.append(url)
-            save_likes(likes)
-            return jsonify({"message": "Like wurde hinzugefügt."})
-        else:
-            return jsonify({"message": "Like bereits vorhanden."})
-    else:
-        return jsonify({"error": "please only submit youtube id's"})
+#╔════════ Variables ════════╗
 
 
-@app.route('/beta/delete_like/<path:id>')
-def delete_like(id):
-    if not "https://www.youtube.com/watch" in id:
-        url = f"https://www.youtube.com/watch?v={id}"
-        likes = load_likes()
-        if url in likes:
-            likes.remove(url)
-            save_likes(likes)
-            return jsonify({"message": "Like wurde entfernt."})
-        else:
-            return jsonify({"error": "Like nicht gefunden."})
-    else:
-        return jsonify({"error": "please only submit youtube id's"})
+PORT = 5010
+open_server = False
 
 
-@app.route('/beta/likes')
-def likes():
-    likes = []
-    try:
-        with open("static/likes.txt", 'r') as file:
-            for zeile in file:
-                likes.append(zeile.strip())
-            return jsonify(likes)
+#╚════════ Variables ════════╝
 
-    except Exception as e:
-        return jsonify({"error": str(e)})
+class C:
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
 
+def startup():
+    version = 1.5
+    print(C.GREEN + f"""  _   _            _______    _          
+ | \ | |          |__   __|  | |         
+ |  \| | _____      _| |_   _| |__   ___ 
+ | . ` |/ _ \ \ /\ / / | | | | '_ \ / _ 
+ | |\  |  __/\ V  V /| | |_| | |_) |  __/
+ |_| \_|\___| \_/\_/ |_|\__,_|_.__/ \___|{C.GREEN} 
+
+[*] - DEanozyp - Zombiebattler
+    
+[*] ╔ {C.BLUE}https://github.com/Zombiebattler/NewTube{C.GREEN}     
+[*] ╠ {C.BLUE}https://github.com/Zombiebattler{C.GREEN} 
+[*] ╚ {C.BLUE}https://github.com/DEanozyp{C.GREEN} 
+
+{C.GREEN}[{C.YELLOW}!{C.GREEN}] - Version: {version}
+{C.GREEN}[{C.YELLOW}!{C.GREEN}] - Running on port: {PORT}
+{C.GREEN}[{C.YELLOW}!{C.GREEN}] - Opern Server: {open_server}
+{C.YELLOW}
+╔════════ Console-Log ════════╗
+
+    """)
 @app.route('/beta/<path:id>')
 def download(id):
     url = f"https://www.youtube.com/watch?v={id}"
+    print(f"\n{C.GREEN}[{C.YELLOW}*{C.GREEN}] - Downloading {url} . . .")
     try:
         yt = YouTube(url).streams.get_highest_resolution().download()
+        print(f"\n{C.GREEN}[{C.GREEN}*{C.GREEN}] - downloaded {yt.title()}")
         return jsonify(f"Downloaded {yt.title()}")
     except Exception as e:
+        print(f"\n{C.GREEN}[{C.RED}!{C.GREEN}] - error Downloading video {url}")
+        print(f"\n{C.RED}[!] - {e}")
         return jsonify(f"Error: {e}")
 
 @app.route('/')
@@ -80,9 +64,6 @@ def homepage():
 
 @app.route('/<path:url>')
 def embed_youtube(url):
-    full_url = request.full_path
-
-    print(url)
     if "youtube.com/watch" in url:
         full_url = request.full_path
         if "www." in full_url:
@@ -96,7 +77,20 @@ def embed_youtube(url):
     else:
         video_id = url
     embed_code = f'<iframe width="100%" height="100%" src="https://www.youtube.com/embed/{video_id}" frameborder="0" allowfullscreen></iframe>'
+    try:
+        yt = YouTube(f"https://www.youtube.com/watch?v={video_id}")
+        print(f"\n{C.GREEN}[{C.YELLOW}*{C.GREEN}] - {yt.title}")
+    except Exception:
+        print(f"\n{C.GREEN}[{C.RED}!{C.GREEN}] - Youtube Title Not Found")
     return render_template('index.html', video_embed=embed_code)
 
 if __name__ == '__main__':
-    app.run(port=5010)#, host="0.0.0.0")
+    startup()
+    try:
+        if open_server == True:
+            app.run(port=PORT, host="0.0.0.0")
+        else:
+            app.run(port=PORT)
+    except Exception:
+        print(f"\n{C.RED}[!] - Error while open server\n{C.YELLOW}Try to change the port")
+    print("Thanks for using NewTube")
